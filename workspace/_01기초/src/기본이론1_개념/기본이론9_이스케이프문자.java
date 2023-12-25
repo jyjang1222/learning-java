0075ed478e9d04cb60ca60f03befa19233fb26a5
Jun <jyjang1222@naver.com>
2023-12-25 22:30:01 +0900

    update

---
+++ workspace/_01기초/src/기본이론1_개념/기본이론9_이스케이프문자.java
@@ -0,0 +1,20 @@
+package 기본이론1_개념;
+
+public class 기본이론9_이스케이프문자 {
+	public static void main(String[] args) {
+		/*
+		이스케이프 문자 \
+		
+		(1) \알파벳 = 기능
+		(2) 종류
+			\n = new line(줄바꿈)
+			\t = tab
+			\"
+			\'
+		*/
+		System.out.println("안녕\n하세요");
+		System.out.println("안녕\t하세요");
+		System.out.println("\"Hi\"");
+		System.out.println("\'Hi\'");
+	}
+}
