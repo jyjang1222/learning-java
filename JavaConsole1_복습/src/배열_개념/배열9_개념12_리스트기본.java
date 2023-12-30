21d5829f2e7015f52ab97d84e0194cce71472e6e
Jun <jyjang1222@naver.com>
2023-12-30 17:21:05 +0900

    update

---
+++ JavaConsole1_복습/src/배열_개념/배열9_개념12_리스트기본.java
@@ -0,0 +1,31 @@
+package 배열_개념;
+
+import java.util.ArrayList;
+
+public class 배열9_개념12_리스트기본 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		list.add(1);
+		list.add(2);
+		list.add(4);
+		
+		System.out.println(list);
+		System.out.println("size : " + list.size());
+				
+		for (int num : list) {
+			System.out.println(num);
+		}
+		
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+			if (list.get(i) == 4) {
+				list.set(i, 3);
+			}
+		}
+		
+		System.out.println(list);
+	}
+
+}
---
+++ JavaConsole1_복습/src/조건문_개념/조건문3_개념01_랜덤.java
@@ -0,0 +1,13 @@
+package 조건문_개념;
+
+import java.util.Random;
+
+public class 조건문3_개념01_랜덤 {
+
+	public static void main(String[] args) {
+		Random ran = new Random();
+		int rNum = ran.nextInt(3) + 1;
+		
+	}
+
+}
