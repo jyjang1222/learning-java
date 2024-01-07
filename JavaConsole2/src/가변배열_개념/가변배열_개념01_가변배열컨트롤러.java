00d58306665e1b1b0847bc8f5620a9cfb83efe35
Jun <jyjang1222@naver.com>
2024-01-07 16:14:54 +0900

    update

--- JavaConsole2/src/가변배열_개념/가변배열_개념01_가변배열컨트롤러.java
+++ JavaConsole2/src/가변배열_개념/가변배열_개념01_가변배열컨트롤러.java
@@ -1,86 +1,161 @@
 package 가변배열_개념;
 
 import java.util.Scanner;
-
+/*
+# 배열 컨트롤러(무한배열) : 벡터(Vector)
+	1. 추가
+		값을 입력받아 순차적으로 추가
+	2. 삭제(인덱스)
+		인덱스를 입력받아 해당 위치의 값 삭제
+	3. 삭제(값)
+		값을 입력받아 삭제
+		없는 값 입력 시 에외처리
+	4. 삽입
+		인덱스와 값을 입력받아 삽입
+*/
+// 교재
 public class 가변배열_개념01_가변배열컨트롤러 {
 
 	public static void main(String[] args) {
-	/*
-		# 배열 컨트롤럴(가변배열) : 벡터(Vector), 연결리스트(ArrayList)
-			1. 추가
-				값을 입력받아 순차적으로 추가
-			2. 삭제(인덱스)
-				인덱스를 입력받아 해당 위치의 값 삭제
-			3. 삭제(값)
-				값을 입력받아 삭제
-				없는 값 입력 시 에외처리
-			4. 삽입
-				인덱스와 값을 입력받아 삽입
-	*/
 		Scanner scan = new Scanner(System.in);
-		int[] score = new int[0];
 		
-		while (true) {
-			for (int i = 0; i < score.length; i++) {
-				System.out.print("[" + score[i] + "]");
+		int[] score = null;
+		int count = 0;
+		
+		while(true) {
+			
+			for(int i=0; i<count; i++) {
+				System.out.print(score[i] + " ");
 			}
 			System.out.println();
-			System.out.println("[벡터 컨트롤러]");
-			System.out.println("[1] 추가");
-			System.out.println("[2] 삭제(인덱스)");
-			System.out.println("[3] 삭제(값)");
-			System.out.println("[4] 삽입");
-			System.out.println("[0] 종료");
-			System.out.print("메뉴 선택 : ");
 			
-			int inputNum = scan.nextInt();
-			// [1]추가
-			if (inputNum == 1) {
-				int[] temp = score;
-				score = new int[score.length + 1];
-				
-				for (int i = 0; i < temp.length; i++) {
-					score[i] = temp[i];
+			System.out.println("[벡터 컨트롤러]");
+			System.out.println("[1]추가");
+			System.out.println("[2]삭제(인덱스)");
+			System.out.println("[3]삭제(값)");
+			System.out.println("[4]삽입");
+			System.out.println("[0]종료");
+			
+			System.out.print("메뉴 선택 : ");
+			int sel = scan.nextInt();
+			
+			if(sel == 0) {
+				 break;
+			} else if(sel == 1) {
+				if(count == 0) {
+					score = new int[count + 1];
+				} else {
+					int[] temp = score;
+					
+					score = new int[count + 1];
+					for(int i=0; i<count; i++) {
+						score[i] = temp[i];
+					}
+					temp = null;
 				}
 				
-				System.out.print("값을 입력 : ");
-				int inputVal = scan.nextInt();
-				score[score.length - 1] = inputVal;
-			// [4]삽입
-			} else if (inputNum == 4) {
-				// 데이터가 존재하면 삽입 가능
-				if (score.length > 0) {
-					System.out.print("삽입할 위치(인덱스) 입력 : ");
-					int insertIdx = scan.nextInt();
+				System.out.print("[추가]성적 입력 : ");
+				int data = scan.nextInt();
+				
+				score[count] = data;
+				count += 1;
+			} else if(sel == 2) {
+				System.out.print("[삭제]인덱스 입력 : ");
+				int index = scan.nextInt();
+				
+				if(count <= index || index < 0) {
+					System.out.println("[해당 위치는 삭제할 수 없습니다.]");
+					continue;
+				}
+				
+				if(count == 1) {
+					score = null;
+				} else if(count > 1) {
+					int[] temp = score;
 					
-					if (0 < insertIdx && insertIdx < score.length) {
-						System.out.print("삽입할 값 입력 : ");
-						int insertVal = scan.nextInt();
-						int[] temp = score;
-						score = new int[score.length + 1];
-
-						int j = 0;
-						for (int i = 0; i < score.length; i++) {
-							if (i == insertIdx) {
-								score[i] = insertVal;
-								continue;
-							}
+					score = new int[count - 1];
+					int j = 0;
+					for(int i=0; i<count; i++) {
+						if(i != index) {
+							score[j] = temp[i];
+							j += 1;
+						}
+					}
+					
+					temp = null;
+				}
+				
+				count -= 1;
+			} else if(sel == 3) {
+				System.out.print("[삭제]값 입력 : ");
+				int data = scan.nextInt();
+				
+				int index = -1;
+				for(int i=0; i<count; i++) {
+					if(score[i] == data) {
+						index = i;
+						break;
+					}
+				}
+				
+				if(index == -1) {
+					System.out.println("[입력하신 값은 존재하지 않습니다.]");
+					continue;
+				} 
+				
+				if(count == 1) {
+					score = null;
+				} else {
+					int[] temp = score;
+					
+					score = new int[count - 1];
+					int j = 0;
+					for(int i=0; i<count; i++) {
+						if(i != index) {
+							score[j] = temp[i];
+							j += 1;
+						}
+					}
+					
+					temp = null;
+				}
+				
+				count -= 1;
+			} else if(sel == 4) {
+				System.out.print("[삽입]인덱스 입력 : ");
+				int index = scan.nextInt();
+				
+				if(count < index || index < 0) {
+					System.out.println("[해당 위치에는 삽입할 수 없습니다.]");
+					continue;
+				}
+				
+				System.out.println("[삽입]값 입력 : ");
+				int data = scan.nextInt();
+				
+				if(count == 0) {
+					score = new int[count + 1];
+				} else if(count > 0) {
+					int[] temp = score;
+					
+					score = new int[count + 1];
+					int j = 0;
+					for(int i=0; i<count + 1; i++) {
+						if(i != index) {
 							score[i] = temp[j];
 							j += 1;
 						}
-					} else {
-						System.out.println("해당 위치에는 삽입할 수 없습니다.");
 					}
-				} else {
-					System.out.println("데이터가 하나 이상 존재해야 삽입가능합니다.");
+					
+					temp = null;
 				}
-			// [0] 종료
-			} else if (inputNum == 0) {				
-				break;
+				
+				score[index] = data;
+				count +=1;
 			}
 		}
 		
-		
+		scan.close();
 	}
 
 }
---
+++ JavaConsole2/src/가변배열_개념/가변배열_개념01_가변배열컨트롤러2.java
@@ -0,0 +1,126 @@
+package 가변배열_개념;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+// 객체지향
+class Controller {
+	int[] score = new int[0];
+	int curCommand = 0;
+	
+	void printMenu() {
+		System.out.println("[벡터 컨트롤러]");
+		System.out.println("[1] 추가");
+//		System.out.println("[2] 삭제(인덱스)");
+//		System.out.println("[3] 삭제(값)");
+		System.out.println("[4] 삽입");
+		System.out.println("[0] 종료");
+		System.out.print("메뉴 선택 : ");
+	}
+	
+	void printData() {
+		System.out.println("컨트롤러 저장소 : " + Arrays.toString(score));
+	}
+	
+	void addData(int val) {
+		int[] temp = score;
+		score = new int[score.length + 1];
+		
+		for (int i = 0; i < temp.length; i++) {
+			score[i] = temp[i];
+		}
+		
+		score[score.length - 1] = val;
+	}
+	
+	boolean checkExistData() {
+		boolean check = false;
+		if (score.length > 0) {
+			check = true;
+		} else {
+			System.out.println("데이터가 하나 이상 존재해야 합니다.");
+		}
+		return check;
+	}
+	
+	boolean checkValidIdx(int idx) {
+		boolean check = false;
+		if (0 <= idx && idx < score.length) {
+			check = true;
+		} else {
+			System.out.println("해당 위치에는 할 수 없습니다.");
+		}
+		return check;
+	}
+	
+	void init() {
+		Scanner scan = new Scanner(System.in);
+		
+		while(true) {
+			printData();
+			printMenu();
+			
+			curCommand = scan.nextInt();
+			
+			if (curCommand == 0) {
+				break;
+			}
+			if (curCommand == 1) {
+				System.out.print("추가할 값을 입력 : ");
+				int inputVal = scan.nextInt();
+				addData(inputVal);
+			} 
+			if (curCommand == 4) {
+				if(checkExistData()) {
+					System.out.print("삽입할 위치(인덱스) 입력 : ");
+					int insertIdx = scan.nextInt();
+					
+					if(checkValidIdx(insertIdx)) {
+						System.out.print("삽입할 값 입력 : ");
+						int insertVal = scan.nextInt();
+						
+						int[] temp = score;
+						score = new int[score.length + 1];
+
+						int j = 0;
+						for (int i = 0; i < score.length; i++) {
+							if (i == insertIdx) {
+								score[i] = insertVal;
+								continue;
+							}
+							score[i] = temp[j];
+							j += 1;
+						}
+					}
+				}
+			}
+			
+		} //end while
+		
+		scan.close();
+	} //end init
+	
+}
+
+public class 가변배열_개념01_가변배열컨트롤러2 {
+	
+	public static void main(String[] args) {
+	/*
+		# 배열 컨트롤러(가변배열) : 벡터(Vector), 연결리스트(ArrayList)
+			1. 추가
+				값을 입력받아 순차적으로 추가
+			2. 삭제(인덱스)
+				인덱스를 입력받아 해당 위치의 값 삭제
+			3. 삭제(값)
+				값을 입력받아 삭제
+				없는 값 입력 시 에외처리
+			4. 삽입
+				인덱스와 값을 입력받아 삽입
+	*/
+		Controller controller = new Controller();
+		
+		controller.init();
+		
+	}
+
+}
