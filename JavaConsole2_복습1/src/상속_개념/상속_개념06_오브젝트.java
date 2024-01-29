84290788049cc425dac0b699df902bd58571a37c
Jun <jyjang1222@naver.com>
2024-01-29 20:01:48 +0900

    update

---
+++ JavaConsole2_복습1/src/상속_개념/상속_개념06_오브젝트.java
@@ -0,0 +1,23 @@
+package 상속_개념;
+
+class ExtendsTest1 extends Object {}
+class ExtendsTest2 {}
+
+class TestA {
+	int a;
+}
+
+public class 상속_개념06_오브젝트 {
+
+	public static void main(String[] args) {
+		TestA a = new TestA();
+		// 자바의 모든 클래스들은 자바에서 미리제공하는 Object 클래스를 내부적으로 자동으로 상속받게 되어있다. 
+		// 변수에 .을 찍어보면 알수있다. 
+//		a.
+		ExtendsTest1 t1 = new ExtendsTest1();
+		ExtendsTest2 t2 = new ExtendsTest2();
+//		t1.
+//		t2.
+	}
+
+}
