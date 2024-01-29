83e0da321310016749ef43a4fbbb8bd51950376c
Jun <jyjang1222@naver.com>
2024-01-29 16:18:07 +0900

    update

--- JavaConsole2_복습1/src/상속_개념/상속_개념02_기본이론2.java
+++ JavaConsole2_복습1/src/상속_개념/상속_개념02_기본이론2.java
@@ -1,6 +1,6 @@
 package 상속_개념;
 
-class A1 {
+class A1  {
 	int a;
 	int b;
 }
@@ -12,30 +12,23 @@ class B1 {
 }
 
 class C1 extends A1 {
-//	int a;
-//	int b;
 	int e;
 	int f;
 }
-
 /*
  * 상속 : 자식클래스 extends(키워드) 부모클래스 {}
  * 상속을 받게되면 마치 자기클래스안의 변수처럼 자유롭게 사용할수있다. 
  * 상속은 한번만 받을수있다. 
  */
-
 public class 상속_개념02_기본이론2 {
 
 	public static void main(String[] args) {
-		
 		A1 A = new A1();
-		
 		B1 B = new B1();
-		B.A.b = 10;
+		B.A.a = 10;
 		
-		C1 C = new C1();	
+		C1 C = new C1();
 		C.b = 10;
-		
 	}
 
 }
