bdd61b224a348839b531133eb9ae0387a7fe8f2d
Jun <jyjang1222@naver.com>
2024-01-31 16:37:33 +0900

    update

---
+++ JavaConsole2_복습1/src/추상화_개념/추상화_개념02_기본이론2.java
@@ -0,0 +1,18 @@
+package 추상화_개념;
+
+abstract class A {
+	abstract void test();
+	void test2() {}
+}
+class B extends A {
+	@Override
+	void test() {}
+}
+
+public class 추상화_개념02_기본이론2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
