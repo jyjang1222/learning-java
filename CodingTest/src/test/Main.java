244a0bd655f5d417d005e5205a42444c0a743021
Jun <jyjang1222@naver.com>
2024-02-18 11:34:27 +0900

    update

--- CodingTest/src/test/Main.java
+++ CodingTest/src/test/Main.java
@@ -1,16 +1,22 @@
 package test;
 
 import java.util.Scanner;
-//import java.util.Arrays;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-    	Scanner scan = new Scanner(System.in);
-    	String id = scan.nextLine();
     	
-    	System.out.println(id + "??!");
- 
+    	Scanner scan = new Scanner(System.in);
+    	int testNum = scan.nextInt();
+    	scan.nextLine();
+    	
+    	for (int i = 0; i < testNum; i++) {
+    		String[] numList = scan.nextLine().split(" ");
+    		int num1 = Integer.parseInt(numList[0]);
+    		int num2 = Integer.parseInt(numList[1]);
+    		int res = num1 + num2;
+    		System.out.println(res);    		
+    	}
+    	
+    	scan.close();
     }
 }
